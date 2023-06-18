38f15b3539054cc3497380eb3b7d0b6715b99cae
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-06-18 19:59:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function submitForm(event) {
     event.preventDefault();
-    var password = document.getElementById('passwordInput').value;
-
-    // Replace 'YourPassword' with the actual password you want to use
-    if (password.toLowerCase() === 'petkstorage') {
-        window.location.href = 'home.html';
+    
+    const passwordInput = document.getElementById('passwordInput');
+    const message = document.getElementById('message');
+    
+    // Add your password validation logic here
+    const password = 'yourpassword'; // Replace with your actual password
+    
+    if (passwordInput.value === password) {
+        // Password is correct
+        message.textContent = 'Password correct. Redirecting...';
+        message.style.color = 'green';
+        message.style.display = 'block';
+        
+        // Delay for the transition effect
+        setTimeout(function() {
+            document.getElementById('passwordPage').style.display = 'none';
+            document.getElementById('homePage').style.display = 'block';
+        }, 1000); // Adjust the delay as needed
     } else {
-        document.getElementById('message').textContent = 'Incorrect password. Please try again.';
-        document.getElementById('message').style.display = 'block';
+        // Password is incorrect
+        message.textContent = 'Password incorrect. Please try again.';
+        message.style.color = 'red';
+        message.style.display = 'block';
     }
 }
