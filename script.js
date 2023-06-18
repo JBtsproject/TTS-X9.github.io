cdf2959151f2f3d8720859695de83dfb762a4627
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-06-18 19:07:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function submitForm(event) {
+    event.preventDefault();
+    var password = document.getElementById('passwordInput').value;
+    
+    // Replace 'your-password' with the actual password you want to use
+    if (password === 'your-password') {
+        window.location.href = 'home.html';
+    } else {
+        document.getElementById('message').textContent = 'Incorrect password. Please try again.';
+        document.getElementById('message').style.display = 'block';
+    }
+}
