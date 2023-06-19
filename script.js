8669d0d3caf03645752fe8596309cda3f0bb8e14
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-06-19 08:36:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function submitForm(event) {
-    event.preventDefault();
-    
-    const passwordInput = document.getElementById('passwordInput');
-    const message = document.getElementById('message');
-    
-    // Add your password validation logic here
-    const password = 'Petkstorage'; // Replace with your actual password
-    
-    if (passwordInput.value === password) {
-        // Password is correct
-        message.textContent = 'Password correct. Redirecting...';
-        message.style.color = 'green';
-        message.style.display = 'block';
-        
-        document.getElementById('passwordPage').style.display = 'none';
-        document.getElementById('homePage').style.display = 'block';
-    } else {
-        // Password is incorrect
-        message.textContent = 'Password incorrect. Please try again.';
-        message.style.color = 'red';
-        message.style.display = 'block';
-    }
+  event.preventDefault();
+
+  const passwordInput = document.getElementById('passwordInput');
+  const message = document.getElementById('message');
+
+  // Add your password validation logic here
+  const password = 'Petkstorage'; // Replace with your actual password
+
+  if (passwordInput.value === password) {
+    // Password is correct
+    message.textContent = 'Password correct. Redirecting...';
+    message.style.color = 'green';
+    message.style.display = 'block';
+
+    setTimeout(() => {
+      window.location.href = 'homepage.html'; // Redirect to homepage.html
+    }, 2000); // Delay for 2 seconds before redirecting
+  } else {
+    // Password is incorrect
+    message.textContent = 'Password incorrect. Please try again.';
+    message.style.color = 'red';
+    message.style.display = 'block';
+  }
 }
