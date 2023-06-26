9b67bf3c9759c157f1703f2f773f2d5c678af460
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-06-26 16:44:53 +0700

    Update HomepageScript.js

--- HomepageScript.js
+++ HomepageScript.js
@@ -7,22 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
   const textWidth = textElement.clientWidth;
   const textHeight = textElement.clientHeight;
 
-  let posX = Math.random() * (containerWidth - textWidth);
-  let posY = Math.random() * (containerHeight - textHeight);
-  let dx = 2;
-  let dy = 2;
+  let posX = containerWidth / 2 - textWidth / 2;
+  let posY = containerHeight / 2 - textHeight / 2;
+  let t = 0;
 
   function animateText() {
-    posX += dx;
-    posY += dy;
-    
-    if (posX + textWidth >= containerWidth || posX <= 0) {
-      dx = -dx;
-    }
+    t += 0.01;
+    const offsetX = containerWidth / 2 - textWidth / 2;
+    const offsetY = containerHeight / 2 - textHeight / 2;
+    const radiusX = containerWidth / 4;
+    const radiusY = containerHeight / 4;
 
-    if (posY + textHeight >= containerHeight || posY <= 0) {
-      dy = -dy;
-    }
+    posX = offsetX + radiusX * Math.cos(t);
+    posY = offsetY + radiusY * Math.sin(t);
 
     textElement.style.transform = `translate(${posX}px, ${posY}px)`;
 
