ebc7f6b47374fa30e25e44cfb371d4509341e023
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-07-14 20:13:34 +0700

    Update HomepageScript.js

--- HomepageScript.js
+++ HomepageScript.js
@@ -1,12 +1,1 @@
-function updateClock() {
-  const clockElement = document.getElementById('clock');
-  const now = new Date();
-  const options = { timeZone: 'Asia/Jakarta', hour12: false, hour: 'numeric', minute: 'numeric', second: 'numeric' };
-  const timeString = now.toLocaleTimeString('en-US', options);
-  clockElement.textContent = timeString;
-}
-
-// Update the clock every second
-setInterval(updateClock, 1000);
-
 
