c09855c6c04c08cfdd2fd3b3e187e775579a40c4
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-07-29 11:57:58 +0700

    Update HomepageScript.js

--- HomepageScript.js
+++ HomepageScript.js
@@ -1,1 +1,20 @@
+// Original JavaScript function from your provided code
+function redirectToTemporaryLink() {
+  window.location.href = 'https://drive.google.com/drive/u/1/folders/1KAolDOGHRx__WSmkvDNF_tEHRjFdg133';
+}
 
+// Function to toggle the dropdown menu
+function toggleMenu() {
+  var dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
+}
+
+// Close the dropdown menu if clicked outside
+window.onclick = function (event) {
+  if (!event.target.matches('.menu-button')) {
+    var dropdownMenu = document.getElementById('dropdownMenu');
+    if (dropdownMenu.style.display === 'block') {
+      dropdownMenu.style.display = 'none';
+    }
+  }
+};
