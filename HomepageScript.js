2deaad66b16697e78f5045c99eac04c5704bbcf7
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-07-29 12:20:20 +0700

    Update HomepageScript.js

--- HomepageScript.js
+++ HomepageScript.js
@@ -1,15 +1,24 @@
-// Original JavaScript function from your provided code
 function redirectToTemporaryLink() {
   window.location.href = 'https://drive.google.com/drive/u/1/folders/1KAolDOGHRx__WSmkvDNF_tEHRjFdg133';
 }
 
-// Function to toggle the dropdown menu
+function redirectToHomePage() {
+  window.location.href = 'homepage.html'; // Replace 'homepage.html' with the filename of your homepage
+}
+
+function redirectToJadwalTugasPage() {
+  window.location.href = 'jadwaltugas.html'; // Replace 'jadwaltugas.html' with the filename of your Jadwal Tugas page
+}
+
+function redirectToResourcePage() {
+  window.location.href = 'resource.html'; // Replace 'resource.html' with the filename of your Resource page
+}
+
 function toggleMenu() {
   var dropdownMenu = document.getElementById('dropdownMenu');
   dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
 }
 
-// Close the dropdown menu if clicked outside
 window.onclick = function (event) {
   if (!event.target.matches('.menu-button')) {
     var dropdownMenu = document.getElementById('dropdownMenu');
