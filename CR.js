1e8b3978a8dae89cf1c6894ccc812d4457d694f1
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-07-30 12:52:42 +0700

    Update CR.js

--- CR.js
+++ CR.js
@@ -1,1 +1,44 @@
+// Sample data for courses (replace this with your actual data)
+const courses = [
+  { id: 1, name: 'Mathematics', description: 'Learn about numbers and equations' },
+  { id: 2, name: 'Science', description: 'Explore the natural world' },
+  { id: 3, name: 'History', description: 'Discover the past' }
+];
+
+// Function to display the list of courses
+function displayCourses() {
+  const courseList = document.querySelector('.course-list');
+
+  courses.forEach(course => {
+    const courseCard = document.createElement('div');
+    courseCard.classList.add('course-card');
+    courseCard.textContent = course.name;
+    courseCard.addEventListener('click', () => displayCourseDetails(course));
+    courseList.appendChild(courseCard);
+  });
+}
+
+// Function to display course details
+function displayCourseDetails(course) {
+  const courseDetails = document.querySelector('.course-details');
+  courseDetails.innerHTML = `
+    <h2>${course.name}</h2>
+    <p>${course.description}</p>
+  `;
+}
+
+// Event listener for the Create Course button
+const createCourseButton = document.getElementById('createCourseButton');
+createCourseButton.addEventListener('click', () => {
+  const newCourse = {
+    id: courses.length + 1,
+    name: 'New Course',
+    description: 'This is a new course'
+  };
+  courses.push(newCourse);
+  displayCourses();
+});
+
+// Initial function call to display courses
+displayCourses();
 
