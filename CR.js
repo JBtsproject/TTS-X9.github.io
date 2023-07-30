43c1d20c6faca7e70cd514973cad32f9c6393d91
P-Setiawan <69039053+PKSetiawan@users.noreply.github.com>
2023-07-30 13:39:00 +0700

    Update CR.js

--- CR.js
+++ CR.js
@@ -1,8 +1,20 @@
 // Sample data for courses (replace this with your actual data)
 const courses = [
-  { id: 1, name: 'Mathematics', description: 'Learn about numbers and equations' },
-  { id: 2, name: 'Science', description: 'Explore the natural world' },
-  { id: 3, name: 'History', description: 'Discover the past' }
+  { id: 1, name: 'Matematika', description: 'MAPEL MATEMATIKA' },
+  { id: 2, name: 'Fisika', description: 'MAPEL FISIKA' },
+  { id: 3, name: 'Kimia', description: 'MAPEL KIMIA' },
+  { id: 4, name: 'Biologi', description: 'MAPEL BIOLOGI' },
+  { id: 5, name: 'Informatika', description: 'MAPEL INFORMATIKA' },
+  { id: 6, name: 'B.Inggris', description: 'MAPEL B.INGGRIS' },
+  { id: 7, name: 'B.Indonesia', description: 'MAPEL B.INDONESIA' },
+  { id: 8, name: 'Geografi', description: 'MAPEL GEOGRAFI' },
+  { id: 9, name: 'PPKN', description: 'MAPEL PPKN' },
+  { id: 10, name: 'PJOK', description: 'MAPEL PJOK' },
+  { id: 11, name: 'Sejarah', description: 'MAPEL SEJARAH' },
+  { id: 12, name: 'Ekonomi', description: 'MAPEL EKONOMI' },
+  { id: 13, name: 'Sosiologi', description: 'MAPEL SOSIOLOGI' },
+  { id: 14, name: 'Seni Rupa', description: 'MAPEL SENI RUPA' },
+  { id: 15, name: 'Agama', description: 'MAPEL AGAMA' }
 ];
 
 // Function to display the list of courses
